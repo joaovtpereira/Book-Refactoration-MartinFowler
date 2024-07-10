90d13c9192996c3f32cf7cc9853e0d6559938d50
joaovtpereira <joaovitorpessoal2@gmail.com>
2024-07-10 18:19:15 -0300

    1 example refactoration

---
+++ chapter1/createStatementData.js
@@ -0,0 +1,97 @@
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay
+    }
+
+    get amount() {
+        throw new Error ("subclass responsaibility")
+    }
+
+    get volumeCredits(){
+        return Math.max(this.performance.audience - 30, 0)
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 0;
+        if(this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30)
+        }
+        return result
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        result  = 30000
+        if(this.performance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result+= 300 * this.performance.audience
+        return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5) 
+    }
+}
+
+
+export default function createStatementData(invoices, plays) {
+    const result = {}
+
+    result.customer = invoices.customer
+    result.performances = invoices.performances.map(enrichPerformance)
+    result.totalAmount = totalAmount(result)
+    result.totalVolumeCredits = totalVolumeCredits(result)
+
+    return result;
+}
+
+function createPerformanceCalculator(aPerformance, aPlay){
+    switch(aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay)
+        default:
+            throw new Error(`unknown type: ${aPlay.type}`)
+    }
+}
+
+function enrichPerformance(aPerformance) {
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance))
+    const result = Object.assign({}, aPerformance)
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
+    
+    return result
+}
+
+function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+}
+
+function amountFor(aPerformance) {
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
+}
+
+function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0)
+    if("comedy" === aPerformance.play.type) 
+        result += Math.floor(aPerformance.audience / 5)
+
+    return result;
+}
+
+function totalAmount(data) {
+    return data.performances
+        .reduce((total, p) => total + p.amount, 0)
+}
+
+function totalVolumeCredits(data) {
+    return data.performances
+        .reduce((total, p) => total + p.volumeCredits, 0)
+}
